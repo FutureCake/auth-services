0ac5fbcb9f8938308a0641a75698daf1ddefb6ed
FutureCake <thomas.c.hoek@gmail.com>
2023-12-11 12:56:17 +0100

    added config contents and manager. replaced hard coded values in index

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import * as https from "https";
+import config from "./configs/configurator";
 import { apply_middlewares, configure_defaults } from "./utils/express.utils";
 
 const service = express();
 configure_defaults(service);
 apply_middlewares(service);
 
-const server = (process.env.NODE_ENV === "prod") ? https.createServer({}, service) : service;
+const server = (config.get('env') === "production") ? https.createServer({}, service) : service;
 
 async function launch_service(): Promise<void> {
 
-    server.listen(3000, 'localhost', () => {
-        console.log(`launched server @ ${'localhost'}:${3000}`);
+    const ip = config.get('server.host');
+    const port = config.get('server.port');
+
+    server.listen(port, ip, () => {
+        console.log(`launched server @ ${ip}:${port}`);
     });
 }
 
