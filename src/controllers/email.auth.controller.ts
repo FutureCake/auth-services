2b401321fb7dc256a28819a4276ba11e76de214b
FutureCake <thomas.c.hoek@gmail.com>
2023-12-11 12:19:57 +0100

    added email auth controller file

---
+++ src/controllers/email.auth.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+async function login(req: Request, res: Response, next: NextFunction): Promise<void> {
+    
+}
+
+async function register(req: Request, res: Response, next: NextFunction): Promise<void> {
+    
+}
+
+async function quick(req: Request, res: Response, next: NextFunction): Promise<void> {
+    
+}
+
+async function trail(req: Request, res: Response, next: NextFunction): Promise<void> {
+    
+}
+
+export {
+    login, quick, register, trail
+};
