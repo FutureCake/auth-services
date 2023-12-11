6b839276d767b4c44e99867bb0a5c8a87c96b51b
FutureCake <thomas.c.hoek@gmail.com>
2023-12-11 12:20:49 +0100

    setup basic express service

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import * as https from "https";
+import { apply_middlewares, configure_defaults } from "./utils/express.utils";
 
 const service = express();
+configure_defaults(service);
+apply_middlewares(service);
 
 const server = (process.env.NODE_ENV === "prod") ? https.createServer({}, service) : service;
 
-async function launch_service() {
+async function launch_service(): Promise<void> {
 
     server.listen(3000, 'localhost', () => {
         console.log(`launched server @ ${'localhost'}:${3000}`);
