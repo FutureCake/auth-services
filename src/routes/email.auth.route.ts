7a1a7b836aa440ecda817219a5d6be750f53a0f1
FutureCake <thomas.c.hoek@gmail.com>
2023-12-11 23:30:52 +0100

    updated email auth routes

--- src/routes/email.auth.route.ts
+++ src/routes/email.auth.route.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import { AllowedSchema } from "express-json-validator-middleware";
 import * as emailAuthController from "../controllers/email.auth.controller";
+import { validate } from "../utils/functions.utils";
+
+const schema: AllowedSchema = {
+    type: "object",
+    required: ["email"],
+    additionalProperties: false,
+    properties: {
+        email: {
+            type: "string",
+            format: "email",
+        }
+    }
+}
 
 const route = express.Router();
 
-route.post('/login', emailAuthController.login);
+route.use(validate({body: schema}));
+
+route.post('/login',  emailAuthController.login);
 route.post('/register', emailAuthController.register);
-route.post('/trail', emailAuthController.trail);
 route.post('/quick-access', emailAuthController.quick);
 
 export default route;
