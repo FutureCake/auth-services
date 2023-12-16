773f8a4ff0befb950bc7138d231b7ac9075574f0
FutureCake <thomas.c.hoek@gmail.com>
2023-12-16 13:32:56 +0100

    added mysql connection code (part of prev commit)

---
+++ src/utils/db.utils.ts
@@ -0,0 +1,29 @@
+import mysql, { PoolOptions } from "mysql2/promise";
+import config from "../configs/configurator";
+
+let db: mysql.Pool;
+
+function connectMYSQL() {
+    if(db === undefined || db === null) {
+
+        const socketPath: PoolOptions = config.get("env") != "production" ? { socketPath: config.get("mysql.socketPath")} : {};
+
+        try {
+            db = mysql.createPool({
+                connectionLimit: 25,
+                host: config.get("mysql.host"),
+                user: config.get("mysql.user"),
+                password: config.get("mysql.password"),
+                database: config.get("mysql.database"),
+                ...socketPath,
+            });    
+        } catch (error) {
+            console.error("failed to connect to db ", error);
+            process.exitCode = 2;
+        }
+    }
+
+    return db;
+}
+
+export default connectMYSQL;
