96c0ead5d115bf70319f6ea7c9282ea3c0584c8f
FutureCake <thomas.c.hoek@gmail.com>
2023-12-16 13:34:26 +0100

    added confirmation code generation util fucntions. moved email connection code to email file (prev commit)

--- src/utils/functions.utils.ts
+++ src/utils/functions.utils.ts
@@ -1,19 +1,51 @@
 import formatsPlugin from "ajv-formats";
 import { Validator } from "express-json-validator-middleware";
-import nodemailer from "nodemailer";
-import config from "../configs/configurator";
+import { randomInt } from "node:crypto";
 
 const validator = new Validator({strict: true});
 formatsPlugin(validator.ajv);
 const validate = validator.validate;
 
-const transporter = nodemailer.createTransport({
-    host: config.get("email.host"),
-    port: config.get("email.port"),
-    auth: {
-        user: config.get("email.user"),
-        pass: config.get("email.pass"),
-    }
-});
+interface CodeOptions {
+    code?: number | string;
+    type?: "numeric" | "alphanumeric" | "alphabetic" | "custom";
+    length?: number;
+    alphabet?: string;
+} 
 
-export { transporter, validate };
+function generateCode(codeOptions?: CodeOptions): string {
+    const options: CodeOptions = {
+        type: "numeric",
+        length: 5,
+        ...codeOptions
+    }
+
+    switch (options.type) {
+        case "alphabetic":
+            return generateFromAlphabet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", options.length!);
+        case "alphanumeric":
+            return generateFromAlphabet("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", options.length!);
+        case "custom":
+            if(!options.alphabet) throw new Error("when using custom an alphabet is required");
+            return generateFromAlphabet(options.alphabet!, options.length!);
+        default:
+            return generateNumeric(options.length!);
+    }
+}
+
+function generateNumeric(length: number) {
+    return randomInt(0, Math.pow(10, length) - 1)
+        .toString()
+        .padStart(length, '0')
+}
+
+function generateFromAlphabet(alphabet: string, length: number): string {
+    return Array(length)
+        .fill('')
+        .map(() => alphabet[randomInt(0, alphabet.length - 1)])
+        .join('')
+}
+
+export { generateCode, validate };
+export type { CodeOptions };
+
