55c19374d07e68d654ab762b39ba5bfceb6014d1
FutureCake <thomas.c.hoek@gmail.com>
2023-12-19 00:14:14 +0100

    added basic service to test db connections etc.

--- src/services/email.auth.service.ts
+++ src/services/email.auth.service.ts
@@ -1,4 +1,9 @@
+import connectMYSQL from "../utils/db.utils";
 
-async function login(email: string) {
-    
+async function loginService(email: string) {
+    const db = connectMYSQL();
+    const [rows, fields] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    console.log(rows, fields);
 }
+
+export { loginService };
