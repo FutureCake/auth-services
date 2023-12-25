aa970640840fc1415570e81861176efe2c847d88
FutureCake <thomas.c.hoek@gmail.com>
2023-12-25 20:10:22 +0100

    rewrote index ts to create a authorizeserivce class for module creation and better code distribution.

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,52 @@ import express from "express";
 import * as https from "https";
 import config from "./configs/configurator";
 import connectMYSQL from "./utils/db.utils";
+import connectEmail from "./utils/email.utils";
 import { applyMiddlewares, configureDefaults, setErrorHandlers, setRoutes } from "./utils/express.utils";
 
-const service = express();
+type Express = typeof express.application;
 
-configureDefaults(service);
-applyMiddlewares(service);
-setRoutes(service);
-setErrorHandlers(service);
-
-const server = (config.get('env') === "production") ? https.createServer({}, service) : service;
-
-async function launch_service(): Promise<void> {
-
-    const ip = config.get('server.host');
-    const port = config.get('server.port');
-
-    connectMYSQL();
-
-    server.listen(port, ip, () => {
-        console.log(`launched server @ ${ip}:${port}`);
-    });
+interface AuthorizeServiceOptions {
+    
 }
 
-launch_service();
+class AuthorizeService<T extends AuthorizeServiceOptions = AuthorizeServiceOptions> {
+
+    private service: Express;
+    private server: Express | https.Server;
+
+    constructor(options: T) {
+        this.service = express();
+        this.server = this.createServer();
+    }
+
+    private createServer(): Express | https.Server {
+        
+        // TODO: set default https options here
+        const options: https.ServerOptions = {};
+
+        return (config.get('env') === "production") ? https.createServer(options, this.service) : this.service
+    }
+
+    public setup(): void {
+        configureDefaults(this.service);
+        applyMiddlewares(this.service);
+        setRoutes(this.service);
+        setErrorHandlers(this.service);
+    }
+
+    public async launch(): Promise<void> {
+
+        const ip = config.get('server.host');
+        const port = config.get('server.port');
+
+        connectMYSQL();
+        connectEmail();
+
+        this.server.listen(port, ip, () => {
+            console.log(`launched server @ ${ip}:${port}`);
+        });
+    }
+}
+
+export default AuthorizeService;
