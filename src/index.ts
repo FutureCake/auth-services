76024c348e855774e432d7780305a22b3f7f915d
FutureCake <thomas.c.hoek@gmail.com>
2023-12-26 22:40:02 +0100

    added aditional config parameters to set properties programmatically.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,69 @@
 import express from "express";
 import * as https from "https";
-import config from "./configs/configurator";
+import { PoolOptions } from "mysql2";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import config, { loadConfigParameters } from "./configs/configurator";
 import connectMYSQL from "./utils/db.utils";
 import connectEmail from "./utils/email.utils";
 import { applyMiddlewares, configureDefaults, setErrorHandlers, setRoutes } from "./utils/express.utils";
 
 type Express = typeof express.application;
 
+interface AuthorizeServiceOptions {
+    config?: {
+        env?: string;
+        authentication?: {
+            jwtToken?: {
+                private?: string;
+                public?: string;
+            };
+        };
+        server?: {
+            port?: number;
+            host?: string;
+            options?: {
+                key?: string;
+                cert?: string;
+            };
+        };
+        email?: {
+            host?: string;
+            port?: number;
+            user?: string;
+            pass?: string;
+        };
+        mysql?: {
+            host?: string;
+            port?: number;
+            user?: string;
+            pass?: string;
+            database?: string;
+            socketPath?: string;
+        };
+    }
+    mysqlOptions?: PoolOptions;
+    emailOptions?: SMTPTransport.Options;
+    serverOptions?: https.ServerOptions;
+}
+
 class AuthorizeService {
 
     private service: Express;
     private server: Express | https.Server;
+    private options?: AuthorizeServiceOptions;
 
-    constructor(options?: unknown) {
+    constructor(options?: AuthorizeServiceOptions) {
+
+        if (options && options.config) loadConfigParameters(options.config);
+
+        this.options = options;
         this.service = express();
         this.server = this.createServer();
     }
 
     private createServer(): Express | https.Server {
-
-        // TODO: set default https options here
-        const options: https.ServerOptions = {};
-
-        return (config.get('env') === "production") ? https.createServer(options, this.service) : this.service;
+        const serverOptions = this.options?.serverOptions ?? {};
+        return (config.get('env') === "production") ? https.createServer(serverOptions, this.service) : this.service;
     }
 
     public setup(): AuthorizeService {
@@ -39,8 +80,8 @@ class AuthorizeService {
         const ip = config.get('server.host');
         const port = config.get('server.port');
 
-        connectMYSQL();
-        connectEmail();
+        connectMYSQL(this.options?.mysqlOptions );
+        connectEmail(this.options?.emailOptions);
 
         this.server.listen(port, ip, () => {
             console.log(`launched server @ ${ip}:${port}`);
@@ -52,3 +93,4 @@ class AuthorizeService {
 new AuthorizeService().setup().launch();
 
 export default AuthorizeService;
+export type { AuthorizeServiceOptions };
